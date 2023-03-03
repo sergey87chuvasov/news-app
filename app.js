c1cb78d3bfa98524522cbb6afaf93840b17dfc88
Sergey87 <serge.techno@gmail.com>
2023-03-03 22:13:27 +0300

    added 1 part of appNews

--- app.js
+++ app.js
@@ -56,10 +56,71 @@ function customHttp() {
 // Init http module
 const http = customHttp();
 
+const newsService = (function () {
+  const apiKey = '9e80f672976c457da7b1606591c0a803';
+  const apiUrl = 'https://newsapi.org/v2';
+
+  // return request to server
+  return {
+    topHeadlines(country = 'ua', cb) {
+      http.get(
+        `${apiUrl}/top-headlines?country=${country}&category=technology&apiKey=${apiKey}`,
+        cb
+      );
+    },
+    everything(query, cb) {
+      http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, cb);
+    },
+  };
+})();
+
 //  init selects
 document.addEventListener('DOMContentLoaded', function () {
+  // for init all plugins of materialize
   M.AutoInit();
+  loadNews();
 });
 
-// Your API key is: 9e80f672976c457da7b1606591c0a803
-// const apiUrl = 'https://news-api-v2.herokuapp.com';
+// basic load news
+function loadNews() {
+  newsService.topHeadlines('ua', onGetResponse);
+}
+
+// fun get respons from server
+function onGetResponse(err, res) {
+  console.log(res);
+  renderNews(res.articles);
+}
+
+// func rend news
+function renderNews(news) {
+  const newsContainer = document.querySelector('.news-container .row');
+  let fragment = '';
+  news.forEach((newsItem) => {
+    const el = newsTemplate(newsItem);
+    fragment += el;
+  });
+  // console.log(fragment);
+  newsContainer.insertAdjacentHTML('afterbegin', fragment);
+}
+
+// news item template func
+function newsTemplate({ urlToImage, title, url, description }) {
+  // console.log(news);
+  return `
+    <div class='col s12'
+      <div class="card">
+        <div class="card-image"
+          <img src="${urlToImage}">
+          <span class="card-title">${title || ''}</span>
+        </div>
+        <div class="card-content">
+          <p>${description || ''}</p>
+        </div>
+        <div class='card-action'>
+          <a href="${url}">Read more</a>
+        </div>
+      </div>
+    </div>
+  `;
+}
